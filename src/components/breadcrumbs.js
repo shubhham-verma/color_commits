4dce126a90f4c280f386dfdececfaf1b5a189e05
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-07-02 02:58:32 +0530

    Fixed breadcrumb no repo hit issue

--- src/components/breadcrumbs.js
+++ src/components/breadcrumbs.js
@@ -8,11 +8,10 @@ export default function Breadcrumbs({ handle_breadcrumb_click,
             {(
                 <div className="flex gap-2 text-sm text-blue-300 mb-4 items-center overflow-x-auto whitespace-nowrap scrollbar-thin">
                     <span
-                        className="cursor-pointer hover:underline"
-                        onClick={() => handle_breadcrumb_click('')}
+                        className={repo_name ? "cursor-pointer hover:underline" : "text-gray-400 cursor-default"}
+                        onClick={repo_name ? () => handle_breadcrumb_click('') : undefined}
                     >
-                        {repo_name || 'Your repo name: '}
-
+                        {repo_name || 'Your repo name'}
                     </span>
                     {current_path.split('/').map((segment, index, arr) => {
                         const fullPath = arr.slice(0, index + 1).join('/');
